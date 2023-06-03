2246ab844a1b524b1df01fb6a148e12304af1576
user <katalonij440@gmail.com>
2023-06-03 12:47:24 +0300

    Update UserItem.jsx

--- src/components/UserList/UserItem/UserItem.jsx
+++ src/components/UserList/UserItem/UserItem.jsx
@@ -49,7 +49,7 @@ export const UserItem = ({ id, user, tweets, followers, avatar }) => {
 
   return (
     <Items key={id} data-aos="fade-up">
-      <LogoImage src="/public/images/logoGoIt.png" alt="logo" />
+      <LogoImage src={process.env.PUBLIC_URL + '/images/logoGoIt.png'} alt="logo" />
       <HorizontalLine />
       <AvatarBox>
         <Avatar
