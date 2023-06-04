0e9d14112ffec37782b2e4a2c91ad001620720b9
user <katalonij440@gmail.com>
2023-06-04 13:40:29 +0300

    fix ?

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { App } from './App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/project-tweets">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
