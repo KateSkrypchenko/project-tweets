bc68945818df3fc3a1c3a2de793cd7168c9e4324
user <katalonij440@gmail.com>
2023-06-04 10:06:16 +0300

    logo

--- src/components/UserList/UserItem/UserItem.jsx
+++ src/components/UserList/UserItem/UserItem.jsx
@@ -49,7 +49,7 @@ export const UserItem = ({ id, user, tweets, followers, avatar }) => {
 
   return (
     <Items key={id} data-aos="fade-up">
-      <LogoImage src={process.env.PUBLIC_URL + '/images/logoGoIt.png'} alt="logo" />
+      <LogoImage src="images/logoGoIT.png" alt="logo" />
       <HorizontalLine />
       <AvatarBox>
         <Avatar
